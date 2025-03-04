724ec75514486afd9f9c8b7b1f5b4f88ab3d658f
variableSoup <iso_code@protonmail.com>
2025-03-04 14:43:05 -0800

    deleted unuused script

--- setup_ELK.sh
+++
@@ -1,8 +0,0 @@
-docker network create elk-network
-docker run -dit --name ubuntu_server --network elk-network ubuntu /bin/bash
-docker inpsect elasticsearch | grep "IPAddress"
-docker stop ubuntu_server
-docker rm ubuntu_server
-docker run -dit --name ubuntu_server --network elk-network -v /var/log:/var/log ubuntu /bin/bash
-docker logs logstash -f
-curl -X GET "http://localhost:9200/_cat/indices?v"
